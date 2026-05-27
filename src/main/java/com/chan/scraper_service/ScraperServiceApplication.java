1902b5f49907bbec2e360e7daa8cd7ec2970f74e
codruthanchevici <codrut.hanchevici@m-ware.eu>
2026-05-27 17:10:19 +0300

    first commit

---
+++ src/main/java/com/chan/scraper_service/ScraperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chan.scraper_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraperServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chan/scraper_service/ScraperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.scraper_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
